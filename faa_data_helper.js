19dd5ce9db460362ff0f0d70cd787532ceb7d17d
Holger Nösekabel <holger@noesekabel.de>
2016-09-26 21:32:38 +0200

    adding network request FAA, status text formatting, tests

---
+++ faa_data_helper.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const _ = require('lodash');
+const rp = require('request-promise');
+const ENDPOINT = 'http://services.faa.gov/airport/status/';
+
+function FAADataHelper() { }
+
+FAADataHelper.prototype.requestAirportStatus = function(airportCode) {
+  return this.getAirportStatus(airportCode)
+  .then(
+    function(response) {
+      console.log('success - received airport info for ' + airportCode);
+      return response.body;
+    }
+  );
+};
+
+FAADataHelper.prototype.getAirportStatus = function(airportCode) {
+  let options = {
+    method: 'GET',
+    uri: ENDPOINT + airportCode,
+    resolveWithFullResponse: true,
+    json: true
+  };
+  return rp(options);
+};
+
+FAADataHelper.prototype.formatAirportStatus = function(airportStatus) {
+  var weather = _.template('The current weather conditions are ${weather}, ${temp} and wind ${wind}.')({
+    weather: airportStatus.weather.weather,
+    temp: airportStatus.weather.temp,
+    wind: airportStatus.weather.wind
+  });
+  if (airportStatus.delay === 'true') {
+    // delay
+    var template = _.template('There is currently a delay for ${airport}. ' +
+      'The average delay time is ${delay_time}. ' +
+      'Delay is because of the following: ${delay_reason}. ${weather}');
+    return template({
+      airport: airportStatus.name,
+      delay_time: airportStatus.status.avgDelay,
+      delay_reason: airportStatus.status.reason,
+      weather: weather
+    });
+  } else {
+    // no delay
+    return _.template('There is currently no delay at ${airport}. ${weather}')({
+      airport: airportStatus.name,
+      weather: weather
+    });
+  }
+};
+module.exports = FAADataHelper;
